69f27ddd1f3de91f973f073147328bc5bd2146f2
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-10-04 20:20:10 -0300

    Creacion de la clase HeapBinaria
    La clase tiene un constructor, en donde se pasa por parámetro la extensión de la heap. Y luego, algunos métodos privados para obtener raíces e hijos.

---
+++ HeapBinaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB
+{
+    class HeapBinaria
+    {
+        int[] datos;
+
+        //Contructor de la HeapBinaria
+
+        public HeapBinaria(int capacidad)
+        {
+            this.datos = new int[capacidad + 1]; //Le sumo ya que considero la heap a partir de la pos 1.
+        }
+        
+        //Metodos para obtener raices e hijos;
+
+        private int getRaiz(){
+            return this.datos[1];
+        }
+
+        private int getDatoPosicion(int i){
+            return this.datos[i];
+        }
+
+        private int getHijoIzquierdo(int i){
+            return this.datos[i * 2];
+        }
+
+        private int getHijoDerecho(int i){
+            return this.datos[i * 2 + 1];
+        }
+
+        private bool tieneHijoIzquierdo(int i)
+        {
+            return this.datos.Length >= (i * 2); 
+        }
+        
+        private bool tieneHijoDerecho(int i)
+        {
+            return this.datos.Length >= (i * 2 + 1);
+        }
+    }
+}
