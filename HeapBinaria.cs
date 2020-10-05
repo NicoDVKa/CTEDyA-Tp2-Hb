a2843378f7ecd9ec535052ca70461b7ca84cc0b4
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-10-04 22:50:23 -0300

    Agrego el método agregarElemento().
    Además, agrego los métodos getPadre(), tienePadre() y swap() para que la función agregarElemento() funcione correctamente.
    También le agrego un atributo a la clase para saber si es una maxHeap o MinHeap.
    Por último, implemento la función verDatos().

--- HeapBinaria.cs
+++ HeapBinaria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HB
@@ -27,6 +28,14 @@ namespace HB
             return int.Parse(this.datos[1].ToString());
         }
 
+        private int getPadre(int i)
+        {
+            return int.Parse(datos[i / 2].ToString());
+        }
+        
+        private bool tienePadre(int i){
+            return i > 1;
+        }
         private int getHijoIzquierdo(int i){
             return int.Parse(this.datos[i * 2].ToString());
         }
@@ -45,6 +54,14 @@ namespace HB
             return this.datos.Length >= (i * 2 + 1);
         }
 
+        //Metodo swap
+        public void swap(int posA , int posB)
+        {
+            T temporal = this.datos[posA];
+            this.datos[posA] = this.datos[posB];
+            this.datos[posB] = temporal;
+        }
+
         //Metodo es vacia
         public bool esVacia()
         {
@@ -52,14 +69,44 @@ namespace HB
         }
 
         //AgregarElementos a la Heap
-        public bool agregarElementos(){
+        public bool agregarElemento(T elemento){
+
+            T[] arregloTemporal = new T[] { elemento };   
+            datos = datos.Concat(arregloTemporal).ToArray();  //Agrego el elemento al final del arreglo
+            int posInsertada = datos.Length - 1;
             
-            
+            if (this.claseHeap) //En caso ser una maxHeap
+            {
+                 while (this.tienePadre(posInsertada) && this.getPadre(posInsertada) < int.Parse(elemento.ToString()))
+                 {
+                    swap(posInsertada, posInsertada / 2);
+                    posInsertada = posInsertada/ 2;
+                 }
+
+                
+            }
+
+            if (!this.claseHeap) //En caso de ser una minHeap
+            {
+                while (this.tienePadre(posInsertada) && this.getPadre(posInsertada) > int.Parse(elemento.ToString()))
+                {
+                    swap(posInsertada, posInsertada / 2);
+                    posInsertada = posInsertada / 2;
+                }
+            }
 
             return true;
         }
 
-
+        //Ver los datos de la Heap
+        public void verDatos()
+        {
+            foreach (T i in this.datos)
+            {
+                Console.Write(i + " " );
+            }
+            Console.WriteLine();
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 namespace HB
@@ -7,14 +8,25 @@ namespace HB
     {
         static void Main(string[] args)
         {
-           int capacidad = -2;                //Si capacidad es < -1 se rompe.
-           if (capacidad < -1) capacidad = -1;
+           int capacidad = -1;                //Si capacidad es < -1 se rompe.
+           if (capacidad < 0) capacidad = 0;
            bool claseHeap = true;          
            HeapBinaria<int> heapBinaria = new HeapBinaria<int>(capacidad,claseHeap);
 
            //Comprobacion de esVacia()
            Console.WriteLine("La heap esta vacia?, {0}", heapBinaria.esVacia());
 
+            heapBinaria.agregarElemento(1);
+            heapBinaria.agregarElemento(6);
+            heapBinaria.agregarElemento(5);
+            heapBinaria.agregarElemento(3);
+            heapBinaria.agregarElemento(4);
+            heapBinaria.agregarElemento(2);
+            heapBinaria.agregarElemento(7);
+            
+
+            heapBinaria.verDatos();
+            Console.WriteLine();
            Console.WriteLine("Presione una tecla para salir.");
            Console.ReadKey(true);
         }
